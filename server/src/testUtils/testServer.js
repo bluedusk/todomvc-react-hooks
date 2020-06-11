a54a5fda2cd38cdaece366c91e6370f33c01b04c
Dan <danielhymn@gmail.com>
2020-06-11 23:31:18 +1000

    add resolver test

--- server/src/testUtils/testServer.js
+++ server/src/testUtils/testServer.js
@@ -3,6 +3,10 @@ import { ApolloServer } from "apollo-server-express";
 import { resolvers } from "../resolvers";
 import { typeDefs } from "../typeDefs";
 
+/**
+ * Similarly we can pass datasource to test client.
+ * @param {*} context
+ */
 export default function testServer(context) {
   return createTestClient(new ApolloServer({ typeDefs, resolvers, context }));
 }
