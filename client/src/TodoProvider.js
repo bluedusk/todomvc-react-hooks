dcebb88a8c80ab168c767d1e5aa0914cdcfe44fe
Dan <danielhymn@gmail.com>
2020-06-28 22:14:23 +1000

    update lib; delete react local context code;

--- client/src/TodoProvider.js
+++
@@ -1,8 +0,0 @@
-import React, { useReducer } from "react";
-import { TodoContext } from "./TodoContext";
-
-export const TodoProvider = ({ reducer, initialState, children }) => (
-	<TodoContext.Provider value={useReducer(reducer, initialState)}>
-		{children}
-	</TodoContext.Provider>
-);
--- client/src/useTodo.js
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import { TodoContext } from "./TodoContext";
-
-export const useTodo = () => useContext(TodoContext);
